91d7e71742ead7d7dd29e74806cf36700cf2e73c
unknown <franswahyu7@gmail.com>
2021-08-28 22:08:47 +0800

    infor dan hot to udah suud cuman masih ada yang kurang

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ document.querySelector('.information').addEventListener('click', function(){
 document.querySelector('.close').addEventListener('click', function(){
     document.querySelector('.display-information').style.display = 'none';
 }) ;
+
+document.querySelector('.howTo').addEventListener('click', function(){
+    document.querySelector('.display-howTo').style.display = 'flex';
+}) ;
+
+document.querySelector('.close-howTo').addEventListener('click', function(){
+    document.querySelector('.display-howTo').style.display = 'none';
+}) ;
