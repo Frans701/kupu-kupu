afe47dfa65fab2af6cdc84080475d9cb04455b22
unknown <franswahyu7@gmail.com>
2021-09-07 11:36:36 +0800

    udah suud problem bagian, tinggi di beberapa menu

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 document.querySelector('.information').addEventListener('click', function(){
     document.querySelector('.display-information').style.display = 'flex';
+    document.querySelector('.main-page').style.position = 'fixed';
 }) ;
 
 document.querySelector('.close').addEventListener('click', function(){
     document.querySelector('.display-information').style.display = 'none';
+    document.querySelector('.main-page').style.position = 'static';
 }) ;
 
 document.querySelector('.howTo').addEventListener('click', function(){
     document.querySelector('.display-howTo').style.display = 'flex';
+    document.querySelector('.main-page').style.position = 'fixed';
 }) ;
 
 document.querySelector('.close-howTo').addEventListener('click', function(){
     document.querySelector('.display-howTo').style.display = 'none';
+    document.querySelector('.main-page').style.position = 'static';
 }) ;
