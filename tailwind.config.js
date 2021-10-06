14783681a6618a755a7a713a2191750460f26c06
Frans701 <franswahyu7@gmail.com>
2021-10-06 11:27:03 +0800

    log in, sigp up done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['index.html', 'Lepidoptera.html', 'Papilionidae.html', 'gallery.html', 'our_top_pick.html', 'newArrival.html'],
+  purge: ['index.html', 'Lepidoptera.html', 'Papilionidae.html', 'gallery.html', 'our_top_pick.html', 'newArrival.html', 'signUp.html', 'logIn.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
